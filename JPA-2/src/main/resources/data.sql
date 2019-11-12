a4cff7fd7e057800e81b2c4986a8e3c7029cb2b7
nexo-group-jbm <pujaribadrinath15@gmail.com>
2019-11-12 15:49:27 +0530

    dlee

--- JPA-2/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-
-INSERT INTO Users (USERNAME, FIRST_NAME, LAST_NAME, EMAIL) 
-	VALUES('wilma', 'Wilma', 'Flintstone', 'wflintstone@example.com');
-INSERT INTO Users (USERNAME, FIRST_NAME, LAST_NAME, EMAIL) 
-	VALUES('fred', 'Fred', 'Flintstone', 'fflintstone@example.com');
-INSERT INTO Users (USERNAME, FIRST_NAME, LAST_NAME, EMAIL) 
-	VALUES('barney', 'Barney', 'Rubble', 'brubble@example.com');
-INSERT INTO Users (USERNAME, FIRST_NAME, LAST_NAME, EMAIL) 
-	VALUES('betty', 'Betty', 'Rubble', 'brubble2@example.com');
--- crudexample/src/main/java/com/example/crudexample/exceptions/ProductNotfoundException.java
+++
@@ -1,5 +0,0 @@
-package com.example.crudexample.exceptions;
-
-public class ProductNotfoundException extends RuntimeException {
-	   private static final long serialVersionUID = 1L;
-	}
