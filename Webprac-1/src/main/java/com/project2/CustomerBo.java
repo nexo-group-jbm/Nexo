732a43c06186271cb4f2dccd6b7c1d97229d2258
nexo-group-jbm <pujaribadrinath15@gmail.com>
2019-11-12 15:44:40 +0530

    Webprac-1 del

--- Webprac-1/src/main/java/com/project2/CustomerBo.java
+++
@@ -1,8 +0,0 @@
-package com.project2;
-
-public interface CustomerBo {
- void addCustomer();
- String addCustomerReturnValue();
- void addCustomerThrowException() throws Exception;
- void addCustomerAround(String name);
-}
